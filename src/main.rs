bee8063ed0a262fbcce19b5b879e3914e6fa0aa4
tanmoyyysarkar <tanmoysarkar.ts1234@gmail.com>
2026-04-23 23:06:35 +0530

    Characters can be added when running the program, crossterm dependency added

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hello, world!");
+use std::io::{self, Read};
+
+fn main(){
+    for b in io::stdin().bytes() {
+        let c = b.unwrap() as char;
+        println!("{}", c);
+        if c == 'q' {
+            break;
+        }
+    }
 }
