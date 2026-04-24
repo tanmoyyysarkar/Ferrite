0280c22393d28d70fddf722eb8012441e1c705fc
tanmoyyysarkar <tanmoysarkar.ts1234@gmail.com>
2026-04-24 18:49:05 +0530

    Improved error handeling

--- src/editor.rs
+++ src/editor.rs
@@ -1,29 +1,32 @@
+use crossterm::event::{Event::Key, KeyCode::Char, read};
 use crossterm::terminal::{disable_raw_mode, enable_raw_mode};
-use crossterm::event::{read, Event::Key, KeyCode::Char};
 pub struct Editor {}
 
 impl Editor {
     pub fn default() -> Self {
         Editor {}
     }
-    pub fn run(&self) {
-        enable_raw_mode().unwrap();
 
+    pub fn run(&self) {
+        if let Err(err) = self.repl() {
+            panic!("{err:#?}");
+        }
+        print!("Goodbye.\r\n");
+    }
+
+    fn repl(&self) -> Result<(), std::io::Error> {
         loop {
-            match read() {
-                Ok(Key(event)) => {
-                    println!("{event:?} \r");
-                    if let Char(c) = event.code {
-                        if c =='q' {
-                            break;
-                        }
+            enable_raw_mode()?;
+            if let Key(event) = read()? {
+                println!("{event:?} \r");
+                if let Char(c) = event.code {
+                    if c == 'q' {
+                        break;
                     }
-                },
-                Err(err) => println!("Error: {err}"),
-                _ => ()
+                }
             }
         }
-
-        disable_raw_mode().unwrap();
+        disable_raw_mode()?;
+        Ok(())
     }
 }
